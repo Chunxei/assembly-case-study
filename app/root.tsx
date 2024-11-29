61b9827669eeeb31a9b151d96b97c9bcc1976b02
Chunxei <meljatau@gmail.com>
2024-11-29 21:41:57 +0100

    initial app setup

--- app/root.tsx
+++ app/root.tsx
@@ -22,6 +22,7 @@ export const links: Route.LinksFunction = () => [
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
   { rel: "stylesheet", href: stylesheet },
+  { rel: "icon", type: "image/png", href: "/favicon.png"},
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -32,6 +33,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <title>Github Search</title>
       </head>
       <body>
         {children}
